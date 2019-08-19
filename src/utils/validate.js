d0e064d1bf2485e1b4043a7aa78ebebf79cf9d7e
lijunhui <lijunhui@360.cn>
2019-08-19 13:34:44 +0800

    添加随机数

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -14,3 +14,11 @@ export function validUsername(str) {
   const valid_map = ['admin', 'editor']
   return valid_map.indexOf(str.trim()) >= 0
 }
+/**
+ * JS获取n至m随机整数
+ * 琼台博客
+ */
+function rd(n, m) {
+  var c = m - n + 1;
+  return Math.floor(Math.random() * c + n);
+}
